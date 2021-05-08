1ddeabebe01fb09a927abad451a9a263d9ea78a7
AayanBaig-WhitehatJR <71639274+AayanBaig-WhitehatJR@users.noreply.github.com>
2021-05-08 14:18:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function preload(){
 }
 function setup(){
     canvas = createCanvas(300,300)
-    canvas.center()
+    canvas.center() 
     video = createCapture(VIDEO)
     video.hide()
     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5AM54HpIs/model.json', modelLoaded)
     }
     function draw(){
-    image(video, 0, 0, 350, 350)
+    image(video, 0, 0, 300, 300)
     classifier.classify(video, gotResult)
     }
     function modelLoaded(){
